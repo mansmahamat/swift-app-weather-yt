59e64df446d9dd0b9248e990660b08062a4ba893
manmah <mansour.mahamat-salle@kambi.com>
2024-04-26 14:58:20 +0200

    Initial Commit

---
+++ swift-app-weather-yt/swift_app_weather_ytApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_app_weather_ytApp.swift
+//  swift-app-weather-yt
+//
+//  Created by Mansour Mahamat-Salle on 2024-04-26.
+//
+
+import SwiftUI
+
+@main
+struct swift_app_weather_ytApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
